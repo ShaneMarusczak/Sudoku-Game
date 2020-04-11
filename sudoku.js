683d6a1c789a6347aebac4e92be1fc4e81463cfa
ShaneMarusczak <marusczaks@gmail.com>
2020-04-11 09:34:02 -0400

    beautification

--- sudoku.js
+++ sudoku.js
@@ -35,8 +35,8 @@
 			}
 		}
 		timer.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" +
-		(minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" +
-		(seconds > 9 ? seconds : "0" + seconds);
+			(minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" +
+			(seconds > 9 ? seconds : "0" + seconds);
 		timerStart();
 	};
 
